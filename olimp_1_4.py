ec199c89a2fa5813942933ab89915f43048b286f
stigmataA <r.muhutdinov@gmail.com>
2024-10-28 16:42:44 +0300

    Add files via upload

---
+++ olimp_1_4.py
@@ -0,0 +1,34 @@
+N = int(input(f'Количество наборов команд: '))
+M = int(input(f'Требуемое количество команд с манипулятором: '))
+
+if M == 0:
+    print(N)
+    exit()
+
+if M > 9:
+    print(0)
+    exit()
+
+manipulator_digits = {0, 4, 8}
+
+result_counter = 0
+
+for _ in range(N):
+    number = int(input(f'Номер блока команд: '))
+    count = 0
+
+    if number == 0:
+        digits = [0]
+    else:
+        digits = []
+        temp_number = number
+        while temp_number > 0:
+            digit = temp_number % 12
+            if digit in manipulator_digits:
+                count += 1
+            temp_number = temp_number // 12
+
+    if count >= M:
+        result_counter += 1
+
+print(result_counter)
